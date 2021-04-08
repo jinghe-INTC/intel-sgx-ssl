99e955b788891c411879983e7dae93d4765dca60
Jing He <jing.j.he@intel.com>
2021-04-08 10:50:21 -0400

    solve the conflict with socket/fs
    Signed-off-by: Jing He <jing.j.he@intel.com>

--- Linux/sgx/libsgx_tsgxssl/tdefines.h
+++ Linux/sgx/libsgx_tsgxssl/tdefines.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2020 Intel Corporation. All rights reserved.
+ * Copyright (C) 2011-2021 Intel Corporation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -48,10 +48,6 @@ struct timeval
     suseconds_t tv_usec;	// microseconds
 };
 
-struct timespec {
-    time_t   tv_sec;        /* seconds */
-    long     tv_nsec;       /* nanoseconds */
-};
 
 struct timeb
 {
